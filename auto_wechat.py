5ccba71eb36be321d0454f1429f5d7d6a992f9b2
lcp <1140823948@qq.com>
2018-06-17 12:54:44 +0800

    \

--- auto_wechat.py
+++ auto_wechat.py
@@ -10,33 +10,33 @@ def hello_word():
     return "hello"
 
 import itchat
-def get_frinends_and_groups():
+def login(uunum):
    instance = itchat.new_instance()
-   instance.auto_login(picDir="./static/QR.png",loginCallback=after_login(instance))
-   #返回二维码给前端
-   return "登录成功!"
+   instance.auto_login(picDir="/home/lcp/PycharmProjects/auto_wechat/static/QR.png")
+   # 登录完成，把对应微信的实例加入到一个字典中存储
+   global wechat_instances
+   wechat_instances[uunum] = instance
 
-def after_login(instance):
-    #登录完成，把对应微信的实例加入到一个字典中存储
-    user = instance.get_friends()[0]
-    user_name = user['UserName']
-    global wechat_instances
-    wechat_instances[user_name] = instance
+
 import uuid
+#初始化：微信二维码等信息
 @app.route('/init')
-def do_login():
+def init():
     #线程执行后台微信相应程序
     num = uuid.uuid1()
-    t = threading.Thread(target=get_frinends_and_groups())
+    t = threading.Thread(target=login,args=(num))
     t.start()
-    return "<html><body><img src='../static/QR.png'><div style>s%</div></body></html>"%(num)
+    return render_template("main.html",uuid_num=num)
 
 @app.route('/friends')
 def get_friends():
-    friends_groups = wechat_core.get_friends_and_groups_list()
+    uuid = request.args.get("uuid")
+    instance = wechat_instances[uuid]
+    friends_groups = wechat_core.get_friends_and_groups_list(instance)
     for friend in friends_groups:
         print friend
     return friends_groups
+
 
 if __name__=='__main__':
     app.run()
